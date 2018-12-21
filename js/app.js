39ee5e4179e66f5b935ee541362ef85ed7e2694f
KADIM Ahmed <akadim70@gmail.com>
2018-12-21 11:39:18 +0100

    Implementing the Player and Enemy Object*

--- js/app.js
+++ js/app.js
@@ -1,11 +1,19 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // Enemy's Initial location
+    this.x = x;
+    this.y = y;
+
+    // Enemy's speed
+    this.speed = speed;
+
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +22,18 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    
+    // Make sure that the enemies won't leave the edges of the platform
+    if(this.x >= 400) {
+        this.x = 0;
+    }
+    
+    // Check the Collision between the Enemy and the Player
+    if (player.x < this.x + 60 && player.x + 37 > this.x && player.y < this.y + 25 && player.y + 30 > this.y) {
+        player.x = 200;
+        player.y = 380;
+    }
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,13 +44,52 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(x, y) {
+   
+    // The Player's position
+    this.x = x;
+    this.y = y;
+    // The Player's sprite Image
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function () {
+    // Make sure the player doesn't leave the game
+    this.x = ((this.x < 0) ? 400 : ( (this.x > 400) ? 0 : this.x ));
+
+    // Return to the Starting point if the Player reaches  the Sea
+    if(this.y < 60) {
+        this.x = 200;
+        this.y = 380;
+    }
+    else if(this.y > 380) { // Make sure the Player doesn't leave the game
+        this.y = 380;
+    }
+}
+
+Player.prototype.render = function () {
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Move the player according to the keys
+Player.prototype.handleInput = function (direction) {
+    
+    switch(direction) {
+        case 'up' : this.y -= 80; break;
+        case 'down' : this.y += 80; break;
+        case 'left' : this.x -= 100; break;
+        case 'right' : this.x += 100; break;
+    }
+
+}
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
-
+// the Enemies should have a random speed (I think)
+allEnemies = [new Enemy(0, 60, Math.floor(Math.random() * 150)), new Enemy(0, 140, Math.floor(Math.random() * 150)), new Enemy(0, 220, Math.floor(Math.random() * 150))];
+player = new Player(200, 380);
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
